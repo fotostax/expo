d074e27286a6c761a2ee713cd6321f3bbbb4cc21
seppemp <gm@fotostax.com>
2025-01-25 16:22:55 -0500

    Feat: Integrated Solution in Custom Test Screen for testing.

--- apps/bare-expo/CustomTestScreen.tsx
+++ apps/bare-expo/CustomTestScreen.tsx
@@ -2,27 +2,28 @@ import BufferViewer from 'components/BufferViewer';
 import { useGLBufferFrameManager } from 'components/GLBufferFrameManager';
 import { renderYUVToRGB, checkGLError } from 'components/GLContextManager';
 import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
-import React, { useEffect, useState, useCallback, useRef } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
-import { Frame, FrameInternal, runAsync } from 'react-native-vision-camera';
 import {
-  Face,
-  useFaceDetector,
-  FaceDetectionOptions,
-} from 'react-native-vision-camera-face-detector';
+  Frame,
+  FrameInternal,
+  useCameraDevice,
+  useFrameProcessor,
+  Camera,
+} from 'react-native-vision-camera';
 import { Worklets } from 'react-native-worklets-core';
-import { CameraPage } from 'screens/CameraView';
 
 const CustomTestScreen = () => {
   const { initializeContext, addFrame, frames } = useGLBufferFrameManager();
   const [gl, setGL] = useState(null);
   const [currentFrameId, setCurrentFrameId] = useState(0);
-  // State for managing camera visibility and frame processing
   const [isCameraActive, setIsCameraActive] = useState(true);
   const [isProcessing, setIsProcessing] = useState(false);
   const [progYUV, setProgYuv] = useState(null);
   const [vtxBuffer, setvtxBuffer] = useState(null);
   const [frameBuffer, setFrameBuffer] = useState(null);
+
+  const device = useCameraDevice('front');
 
   // Initialize GL context when the component mounts
   useEffect(() => {
@@ -53,21 +54,6 @@ const CustomTestScreen = () => {
       console.error('Error during GL context preparation:', error);
     }
   };
-
-  // Handle screen tap to start frame processing
-  const handleScreenTap = useCallback(() => {
-    if (!isProcessing && gl != null) {
-      setIsProcessing(true);
-      // Stop frame processing and remove the camera afrter 3 seconds
-      setTimeout(() => {
-        setIsProcessing(false);
-        setTimeout(() => {
-          console.log('removing camera...');
-          setIsCameraActive(false); // Render an empty view
-        }, 2000);
-      }, 2500);
-    }
-  }, [isProcessing, gl]);
 
   const yuvToRGBCallback = Worklets.createRunOnJS(async (frame: Frame) => {
     const internal = frame as FrameInternal;
@@ -104,17 +90,45 @@ const CustomTestScreen = () => {
     }
   });
 
-  const renderCallback = async (frame: Frame) => {
-    'worklet';
-    if (isProcessing) {
-      await yuvToRGBCallback(frame);
+  const frameProcessor = useFrameProcessor(
+    async (frame: Frame) => {
+      'worklet';
+      if (isProcessing) {
+        await yuvToRGBCallback(frame);
+      }
+    },
+    [isProcessing]
+  );
+
+  const handleScreenTap = useCallback(() => {
+    if (!isProcessing && gl != null) {
+      setIsProcessing(true);
+      setTimeout(() => {
+        setIsProcessing(false);
+        setTimeout(() => {
+          console.log('removing camera...');
+          setIsCameraActive(false); // Render an empty view
+        }, 2000);
+      }, 2500);
     }
-  };
+  }, [isProcessing, gl]);
 
   return (
     <TouchableOpacity style={styles.container} onPress={handleScreenTap}>
       {isCameraActive ? (
-        <CameraPage style={styles.cameraView} renderCallback={yuvToRGBCallback} isProcessing={isProcessing}/>
+        device ? (
+          <Camera
+            style={styles.camera}
+            device={device}
+            isActive
+            frameProcessor={frameProcessor}
+            resizeMode="contain"
+          />
+        ) : (
+          <View style={styles.emptyContainer}>
+            <Text style={styles.text}>Loading Camera...</Text>
+          </View>
+        )
       ) : (
         <View style={styles.emptyView}>
           <BufferViewer
@@ -131,9 +145,19 @@ const CustomTestScreen = () => {
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: 'white' },
-  cameraView: { flex: 1 },
+  camera: { flex: 1 },
   emptyView: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000' },
-  emptyText: { color: '#fff', fontSize: 16 },
+  text: {
+    color: 'white',
+    fontSize: 11,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default CustomTestScreen;
