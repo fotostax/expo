df2a08dcf6d8082edc4d37c8dab312f0005ed25c
seppemp <gm@fotostax.com>
2025-02-24 16:18:39 -0500

    Feat: Initial FotostackApp clone

---
+++ apps/fotostax-app/e2e/TestSuite-test.native.js
@@ -0,0 +1,28 @@
+/**
+ * The test cases for bare-expo E2E testing.
+ */
+export const TESTS = [
+  'Basic',
+  // 'Asset',
+  // 'FileSystem',
+  // 'Font',
+  'Permissions',
+  // 'Blur',
+  'LinearGradient',
+  'Constants',
+  // 'Contacts',
+  'Crypto',
+  // 'GLView',
+  'Haptics',
+  'Localization',
+  // 'SecureStore',
+  // 'Segment',
+  'SQLite',
+  'Random',
+  'Permissions',
+  'KeepAwake',
+  // 'Audio',
+  'HTML',
+  'FileSystem@next',
+  'Fetch',
+];
