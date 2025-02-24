c9b013151afe4b7539a9e254a9fd5c7edfe657a4
seppemp <gm@fotostax.com>
2025-02-24 18:17:16 -0500

    Feat: Removed Theme provider from expo.

--- apps/fotostax-app/App.tsx
+++ apps/fotostax-app/App.tsx
@@ -1,13 +1,8 @@
+import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
 import React from 'react';
+
 import MainNavigator from './MainNavigator';
-import { ThemeProvider } from 'ThemeProvider'; // Remove if you don’t need theming
 
 export default function App() {
-  return (
-    // Wrap your app in the ThemeProvider if you want themed colors.
-    // Otherwise, you can remove this and import MainNavigator directly.
-    <ThemeProvider>
-      <MainNavigator />
-    </ThemeProvider>
-  );
+  return <MainNavigator />;
 }
--- apps/fotostax-app/MainNavigator.tsx
+++ apps/fotostax-app/MainNavigator.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { useTheme } from 'ThemeProvider'; // Remove if you don't want theming
 import React from 'react';
 import { View, Text } from 'react-native';
 
@@ -9,16 +8,15 @@ const Stack = createStackNavigator();
 // A simple screen that uses theme values for background/text.
 // Feel free to remove theme references if you don’t need them.
 function EmptyScreen() {
-  const { theme } = useTheme(); // Remove if you removed ThemeProvider
   return (
     <View
       style={{
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: theme?.background?.default ?? '#fff',
+        backgroundColor: '#fff',
       }}>
-      <Text style={{ color: theme?.text?.default ?? '#000' }}>Hello from the Empty Screen!</Text>
+      <Text style={{ color: '#000' }}>Hello from the Empty Screen!</Text>
     </View>
   );
 }
