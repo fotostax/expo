d614e4ecd5951fe2c5870ea586e6a1d5f2ee0649
seppemp <gm@fotostax.com>
2025-02-24 18:39:03 -0500

    Feat: Created fotostax clean project with expogl support.

--- apps/fotostax-app/App.tsx
+++
@@ -1,8 +0,0 @@
-import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
-import React from 'react';
-
-import MainNavigator from './MainNavigator';
-
-export default function App() {
-  return <MainNavigator />;
-}
