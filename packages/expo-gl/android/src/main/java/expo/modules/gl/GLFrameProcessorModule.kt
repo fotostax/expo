e1cf2ea04153fe41d0d0f5413cef99fec2bf4dad
seppemp <gm@fotostax.com>
2025-03-31 15:23:10 -0500

    Feat: Fixed bridge elements for app context.

--- packages/expo-gl/android/src/main/java/expo/modules/gl/GLFrameProcessorModule.kt
+++ packages/expo-gl/android/src/main/java/expo/modules/gl/GLFrameProcessorModule.kt
@@ -3,21 +3,24 @@ package expo.modules.gl
 import android.util.Log
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.JavaScriptContextHolder
 
 class GLFrameProcessorModule : Module() {
-  override fun definition() = ModuleDefinition {
-    Name("GLFrameProcessor")
+    override fun definition() = ModuleDefinition {
+        Name("GLFrameProcessor")
 
-    Function("install") {
-      val jsContext = appContext.reactContext?.javaScriptContextHolder
-      if (jsContext != null && jsContext.get() != 0) {
-        registerFrameProcessorPlugin(jsContext.get(), "uploadTexturePlugin")
-      } else {
-        Log.e("GLFrameProcessorModule", "JSI Runtime is not available")
-        throw Exception("JSI Runtime is not available")
-      }
+        Function("install") {
+            val reactContext = appContext.reactContext as? ReactApplicationContext
+            val jsContext = reactContext?.javaScriptContextHolder
+            if (jsContext != null && jsContext.get() != 0L) {
+                registerFrameProcessorPlugin(jsContext.get(), "uploadTexturePlugin")
+            } else {
+                Log.e("GLFrameProcessorModule", "JSI Runtime is not available")
+                throw Exception("JSI Runtime is not available")
+            }
+        }
     }
-  }
 
-  private external fun registerFrameProcessorPlugin(jsiPtr: Long, pluginName: String)
+    private external fun registerFrameProcessorPlugin(jsiPtr: Long, pluginName: String)
 }
